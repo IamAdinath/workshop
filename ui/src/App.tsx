4f1f2e7fa6d94492da438e2c2066bd23944d949f
Adinath Gore <59023488+IamAdinath@users.noreply.github.com>
2025-03-01 12:33:59 +0530

    fix the import sqft page issue (#9)

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import About from './pages/About';
-import SqftCalculator from './pages/SQFTCalculator';
+import SqftCalculator from './pages/SqftCalculator';
 import './styles/global.css';
 
 const App: React.FC = () => {
